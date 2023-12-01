1d03dbc036dc0dea127c141d4d170ffcda48b20f
devgiljong <calf-love@naver.com>
2023-12-01 18:50:03 +0900

    :sparkles: 디바운스 추가

---
+++ packages/react-hook/src/debounce/use-debounce.ts
@@ -0,0 +1,16 @@
+import { useRef } from 'react';
+
+export const useDebounce = <ParamType extends unknown[]>(
+  callback: (...params: ParamType) => void,
+  time: number,
+) => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  return (...params: ParamType) => {
+    if (timer.current) clearTimeout(timer.current);
+
+    timer.current = setTimeout(() => {
+      callback(...params);
+      timer.current = null;
+    }, time);
+  };
+};
--- packages/react-hook/src/index.ts
+++ packages/react-hook/src/index.ts
@@ -1,3 +1,7 @@
-export const hi = () => {
+import { useDebounce } from './debounce/use-debounce';
+
+const hi = () => {
   return 'hello world';
 };
+
+export { useDebounce, hi };
