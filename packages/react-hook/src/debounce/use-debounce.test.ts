15180c593d70c6cdc15dac86839ac1e1213b8b8a
devgiljong <calf-love@naver.com>
2023-12-03 03:32:20 +0900

    :sparkles: 테스트 종속성

--- packages/react-hook/src/debounce/use-debounce.test.ts
+++ packages/react-hook/src/debounce/use-debounce.test.ts
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 describe('test', () => {
   it('hi', () => {
     expect(true).toBe(true);
---
+++ root.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+describe('test', () => {
+  it('hidsadsadas', () => {
+    expect(true).toBe(true);
+  });
+});
