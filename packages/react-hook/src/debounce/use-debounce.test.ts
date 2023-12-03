db216dc7225e09cdcc2f589c695484302a9ae93c
devgiljong <calf-love@naver.com>
2023-12-03 22:49:11 +0900

    :sparkles: 렌더훅 동작 확인

--- packages/react-hook/src/debounce/use-debounce.test.ts
+++ packages/react-hook/src/debounce/use-debounce.test.ts
@@ -1,6 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
+import { useDebounce } from './use-debounce';
 describe('test', () => {
   it('hi', () => {
+    const callback = jest.fn();
+    const { result } = renderHook(() => useDebounce(callback, 100));
+
     expect(true).toBe(true);
   });
 });
