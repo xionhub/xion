e8c769f43670b7716f15cb9b539df15ea79cf8d0
devgiljong <calf-love@naver.com>
2023-12-04 00:06:50 +0900

    :sparkles: 유즈 디바운스 테스트 추가

--- packages/react-hook/src/debounce/use-debounce.test.ts
+++ packages/react-hook/src/debounce/use-debounce.test.ts
@@ -1,10 +1,14 @@
-import { render, renderHook } from '@testing-library/react';
+import { act, render, renderHook } from '@testing-library/react';
 import { useDebounce } from './use-debounce';
 describe('test', () => {
   it('hi', () => {
+    jest.useFakeTimers();
     const callback = jest.fn();
     const { result } = renderHook(() => useDebounce(callback, 100));
-
-    expect(true).toBe(true);
+    act(() => result.current());
+    act(() => result.current());
+    act(() => result.current());
+    jest.advanceTimersByTime(100);
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
