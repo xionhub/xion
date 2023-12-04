2af26d962640f46ea2c05cf63c1fbfe6a4e20e28
devgiljong <calf-love@naver.com>
2023-12-04 13:54:25 +0900

    :sparkles: Debounce 테스트 중복 제거 및 커버리지 측정 100%

--- packages/react-hook/src/debounce/use-debounce.test.ts
+++ packages/react-hook/src/debounce/use-debounce.test.ts
@@ -1,15 +1,32 @@
 import { act, render, renderHook } from '@testing-library/react';
 import { useDebounce } from './use-debounce';
 describe('test use-debounce hook', () => {
-  it('it should be called only one time with last argument', () => {
+  beforeEach(() => {
     jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  it('it should be called only one time with last argument', () => {
     const callback = jest.fn();
-    const { result } = renderHook(() => useDebounce(callback, 100));
+    const { result } = renderHook(() => useDebounce(callback, 10));
     act(() => result.current('param1'));
     act(() => result.current('param2'));
     act(() => result.current('param3'));
-    jest.advanceTimersByTime(100);
+    jest.advanceTimersByTime(10);
     expect(callback).toHaveBeenCalledWith('param3');
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('it should not call callback when time before', () => {
+    const callback = jest.fn();
+    const { result } = renderHook(() => useDebounce(callback, 100));
+    act(() => result.current('param1'));
+    jest.advanceTimersByTime(99);
+    expect(callback).toHaveBeenCalledTimes(0);
+    jest.advanceTimersByTime(1);
     expect(callback).toHaveBeenCalledTimes(1);
   });
 });
