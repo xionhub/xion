7278882a296b99c167247cce77cb63a1472acaf8
devgiljong <calf-love@naver.com>
2023-12-04 09:59:54 +0900

    :sparkles: 테스트 리팩토링 및 테스트 피쳐 추가

--- packages/react-hook/src/debounce/use-debounce.test.ts
+++ packages/react-hook/src/debounce/use-debounce.test.ts
@@ -1,14 +1,15 @@
 import { act, render, renderHook } from '@testing-library/react';
 import { useDebounce } from './use-debounce';
-describe('test', () => {
-  it('hi', () => {
+describe('test use-debounce hook', () => {
+  it('it should be called only one time with last argument', () => {
     jest.useFakeTimers();
     const callback = jest.fn();
     const { result } = renderHook(() => useDebounce(callback, 100));
-    act(() => result.current());
-    act(() => result.current());
-    act(() => result.current());
+    act(() => result.current('param1'));
+    act(() => result.current('param2'));
+    act(() => result.current('param3'));
     jest.advanceTimersByTime(100);
+    expect(callback).toHaveBeenCalledWith('param3');
     expect(callback).toHaveBeenCalledTimes(1);
   });
 });
