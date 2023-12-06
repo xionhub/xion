e42bd7b63ecc039b4805805e35c15338df19b76a
devgiljong <calf-love@naver.com>
2023-12-07 01:07:53 +0900

    :sparkles: Object values add

---
+++ packages/util/src/object-values/object-values.test.ts
@@ -0,0 +1,16 @@
+import { objectValues } from './object-values';
+
+describe('object-values', () => {
+  it('잘 변환하는지 테스트합니다.', () => {
+    const example = {
+      hi: 'hi',
+      bool: false,
+      nested: {
+        bye: 'bye',
+      },
+    };
+
+    const valuesArray = objectValues(example);
+    expect(Array.isArray(valuesArray)).toBe(true);
+  });
+});
---
+++ packages/util/src/object-values/object-values.ts
@@ -0,0 +1,3 @@
+export const objectValues = <Type extends Record<PropertyKey, unknown>>(
+  obj: Type,
+) => Object.values(obj) as Array<Type[keyof Type]>;
