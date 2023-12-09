08c7756788ebcb213d6d05bdc857877fef9a7523
devgiljong <calf-love@naver.com>
2023-12-09 10:40:11 +0900

    :sparkles: Esbuild 설정 외부로

--- configs/build/build.ts
+++ configs/build/build.ts
@@ -1,36 +1,39 @@
 import esbuild, { BuildOptions, SameShape } from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
-export const xionEsBuildConfig = async <T extends BuildOptions>(options:SameShape<BuildOptions, T>) => {
-    const baseConfig = {
-        entryPoints: ['src/index.ts'],
-        outdir: 'dist', 
-        bundle: true,
-        sourcemap: true, 
-        treeShaking: true,
-        minify: true,
+export const xionEsBuildConfig = async <T extends BuildOptions>(
+  options: SameShape<BuildOptions, T>,
+) => {
+  const baseConfig = {
+    entryPoints: ['src/index.ts'],
+    outdir: 'dist',
+    bundle: true,
+    sourcemap: true,
+    treeShaking: true,
+    minify: true,
+    format: 'cjs',
+    plugins: [nodeExternalsPlugin()],
+  };
+  try {
+    return await Promise.all([
+      // 한 번은 cjs
+      esbuild.build({
+        ...baseConfig,
+        ...options,
         format: 'cjs',
-        plugins: [nodeExternalsPlugin()],
-        ...options
-      };
-      try {
-        return await Promise.all([
-            // 한 번은 cjs
-            esbuild.build({
-                ...baseConfig,
-                format: 'cjs',
-                outExtension: {
-                    '.js': '.cjs',
-                },
-            }),
-            // 한 번은 esm
-            esbuild.build({
-                ...baseConfig,
-                format: 'esm',
-            }),
-        ]);
-    } catch {
-        console.log('Build failed');
-        process.exit(1);
-    }      
-}
+        outExtension: {
+          '.js': '.cjs',
+        },
+      }),
+      // 한 번은 esm
+      esbuild.build({
+        ...baseConfig,
+        ...options,
+        format: 'esm',
+      }),
+    ]);
+  } catch {
+    console.log('Build failed');
+    process.exit(1);
+  }
+};
