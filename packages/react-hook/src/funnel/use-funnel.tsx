915c37f225bce3a09203ac469e8eba40d1c69306
devgiljong <calf-love@naver.com>
2023-12-10 11:39:38 +0900

    :sparkles: Funnel test

--- packages/react-hook/src/funnel/use-funnel.tsx
+++ packages/react-hook/src/funnel/use-funnel.tsx
@@ -1,13 +1,70 @@
 import React from 'react';
-import { FunnelOptionObject, NonEmptyArray } from './funnel.type';
-import { RecursivePartial, mergeOptions } from '@xionhub/util';
+import {
+  FunnelOptionObject,
+  NonEmptyArray,
+  RouteFunnelProps,
+  StepProps,
+} from './funnel.type';
+import {
+  RecursivePartial,
+  mergeOptions,
+  safeHistory,
+  safeLocation,
+  safeSearchParams,
+} from '@xionhub/util';
+import { FunnelCore } from './funnel.core';
+import { Funnel, Step } from './funnel';
 
 export const useFunnel = <Steps extends NonEmptyArray<string>>(
   array: Steps,
   userOptions: RecursivePartial<FunnelOptionObject<Steps>> = {},
 ) => {
-  const options = mergeOptions(userOptions, {
+  const { targetKey, initialStep } = mergeOptions(userOptions, {
     initialStep: array[0],
     targetKey: 'step',
   });
+  const funnelCore = new FunnelCore({ safeHistory, safeLocation });
+  const history = safeHistory();
+  const [steps, _] = React.useState<Steps>(array);
+  const [currentStep, setCurrentStep] = React.useState<Steps[number]>(
+    () => initialStep,
+  );
+  const nextStep = (nextQuery: Steps[number]) => {
+    return () => {
+      setCurrentStep(() => {
+        history.pushState(
+          '',
+          '',
+          funnelCore.createNextStep(targetKey, nextQuery),
+        );
+        return nextQuery;
+      });
+    };
+  };
+
+  React.useEffect(() => {
+    const handlePopState = () => {
+      const nowStep = safeSearchParams(targetKey) ?? '';
+      setCurrentStep(nowStep);
+    };
+    window.addEventListener('popstate', handlePopState);
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
+  const FunnelComponent = React.useMemo(() => {
+    // eslint-disable-next-line react/display-name
+    return Object.assign(
+      (props: RouteFunnelProps<Steps>) => {
+        return <Funnel<Steps> step={currentStep} steps={steps} {...props} />;
+      },
+      {
+        Step: (props: StepProps<Steps>) => {
+          return <Step {...props} />;
+        },
+      },
+    );
+  }, [currentStep, steps]);
+  return [FunnelComponent, nextStep] as const;
 };
