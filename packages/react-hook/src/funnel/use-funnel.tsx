666b0382f0009bbdca67cb21f6a8ec9cf2a357ac
devgiljong <calf-love@naver.com>
2023-12-10 11:20:31 +0900

    :sparkles: Funnel

--- packages/react-hook/src/funnel/use-funnel.tsx
+++ packages/react-hook/src/funnel/use-funnel.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
-import { NonEmptyArray } from './funnel.type';
+import { FunnelOptionObject, NonEmptyArray } from './funnel.type';
+import { RecursivePartial, mergeOptions } from '@xionhub/util';
 
-export const useFunnel = <Steps extends NonEmptyArray<string>>(array:Steps , options:) => {};
+export const useFunnel = <Steps extends NonEmptyArray<string>>(
+  array: Steps,
+  userOptions: RecursivePartial<FunnelOptionObject<Steps>> = {},
+) => {
+  const options = mergeOptions(userOptions, {
+    initialStep: array[0],
+    targetKey: 'step',
+  });
+};
