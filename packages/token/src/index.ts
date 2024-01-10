a03d04ed94fed612ec3bbe16132322b519214a2b
devgiljong <calf-love@naver.com>
2024-01-10 16:20:11 +0900

    :sparkles: Token

--- packages/token/src/index.ts
+++ packages/token/src/index.ts
@@ -33,7 +33,7 @@ export const radius = {
   lg: '28px',
   xl: '60px',
   full: '1000px',
-};
+} as const;
 
 type NumberToString<N extends number, R extends unknown[] = []> =
   | `${R['length']}`
@@ -41,19 +41,31 @@ type NumberToString<N extends number, R extends unknown[] = []> =
 
 type NumberString<N extends number> = Exclude<NumberToString<N>, '0'>;
 
-type SpacingKey = NumberString<40>;
+type SpacingKey = NumberString<20>;
 
 export const spacing = Object.fromEntries(
-  Array.from(Array(40)).map(
+  Array.from(Array(20)).map(
     (_, i) => [`${i + 1}`, `${(i + 1) / 4}rem`] as const,
   ),
 ) as {
   [key in SpacingKey]: string;
 };
 
+export const fontWeight = {
+  thin: 100,
+  extraLight: 200,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  extraBold: 800,
+} as const;
+
 export const boxShadow = {};
 
 export const token = {
   color,
   radius,
+  boxShadow,
 };
