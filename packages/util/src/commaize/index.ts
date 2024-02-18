3c225ddd450d2a87559e3366d4f714cd1cde2eb8
devgiljong <calf-love@naver.com>
2024-02-18 14:52:14 +0900

    :sparkles: Commaize

---
+++ packages/util/src/commaize/index.ts
@@ -0,0 +1,6 @@
+export const commaize = (input: number | string): string => {
+  const inputStr = input.toString();
+  const parts = inputStr.split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return parts.join('.');
+};
