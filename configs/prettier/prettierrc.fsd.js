f8af1d657fc7cde9a96b8d3230c949186d7224d6
devgiljong <calf-love@naver.com>
2024-03-30 02:08:03 +0900

    :sparkles: Prettier 설정 추가

---
+++ configs/prettier/prettierrc.fsd.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^[@/]',
+    '^~shared/(.*)$',
+    '^~entities/(.*)$',
+    '^~feature/(.*)$',
+    '^~widget/(.*)$',
+    '^~page/(.*)$',
+    '^~app/(.*)$',
+    '^[./]',
+    '^[../]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
